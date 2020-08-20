39c1870c9dee0f35f42ec9f7a1c75b184001df7b
Rashik Tuladhar <rashiktuladhar2016@outlook.com>
2020-08-20 21:23:23 +0545

    swagger and api versioning added

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using WebApi.Extensions;
 
 namespace WebApi
 {
@@ -26,7 +19,9 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerExtension();
             services.AddControllers();
+            services.AddApiVersioningExtension();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +37,7 @@ namespace WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSwaggerExtension();
 
             app.UseEndpoints(endpoints =>
             {
